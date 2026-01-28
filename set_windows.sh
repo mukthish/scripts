92695815fa718ea373619211d281edf1658a3efb
mukthish <mukthish.2000@gmail.com>
2026-01-28 16:47:21 +0530

    adding grub config file for windows as default

---
+++ set_windows.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo sed -i 's/^GRUB_DEFAULT=.*/GRUB_DEFAULT=saved/' /etc/default/grub
+sudo grub2-set-default "$(sudo awk -F\' '$1=="menuentry " && $2 ~ /Windows Boot Manager/ {print $2}' /boot/grub2/grub.cfg | head -n 1)"
+sudo grub2-mkconfig -o /boot/grub2/grub.cfg
