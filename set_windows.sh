47256d1f70eb8c558012c5b40ac13b0ff97ab516
mukthish <mukthish.2000@gmail.com>
2026-03-18 12:43:04 +0530

    making the settings persistent

--- set_windows.sh
+++ set_windows.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+sudo sed -i 's/^UPDATEDEFAULT=.*/UPDATEDEFAULT=no/' /etc/sysconfig/kernel
 sudo sed -i 's/^GRUB_DEFAULT=.*/GRUB_DEFAULT=saved/' /etc/default/grub
 sudo grub2-set-default "$(sudo awk -F\' '$1=="menuentry " && $2 ~ /Windows Boot Manager/ {print $2}' /boot/grub2/grub.cfg | head -n 1)"
 sudo grub2-mkconfig -o /boot/grub2/grub.cfg
